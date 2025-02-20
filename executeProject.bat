137e5dcb60a7ff8e419849d0d0f19f661ba568fa
김도형 <mrdos@naver.com>
2025-02-20 15:31:17 +0900

    [도형] 도커 빌드용 실행파일 수정

--- executeProject.bat
+++ executeProject.bat
@@ -1,3 +1,11 @@
+docker pull mrdos89/hbgogumaserver:latest
+docker pull mrdos89/hbgoguma-project:latest
+
+cd compose
+docker compose down
+docker compose --env-file .env up -d
+cd ..
+
 call npm install react-icons
 call npm install react-router-dom
 call npm install axios
