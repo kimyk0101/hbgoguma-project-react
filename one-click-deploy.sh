0e2e8913aae782ae62e5dd8053e1635879a7390a
김도형 <mrdos@naver.com>
2025-02-20 15:42:51 +0900

    [도형] 빌드시 필요없는 코드 삭제

--- one-click-deploy.sh
+++ one-click-deploy.sh
@@ -7,17 +7,3 @@ docker tag hbgoguma-project mrdos89/hbgoguma-project
 
 # docker에서 push 올림
 docker push mrdos89/hbgoguma-project
-
-# compose 폴더에서 .env에 맞춰 docker compose 실행
-cd compose
-docker compose --env-file .env up -d
-cd ..
-
-# 필요한 패키지 설치
-npm install react-icons
-npm install react-router-dom
-npm install axios
-npm install react-dom
-
-# 프론트엔드 실행
-npm install && npm run dev
