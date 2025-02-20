eab5d8e620e7e0daa0f36af744cb54fd2005b1e0
김도형 <mrdos@naver.com>
2025-02-20 15:42:20 +0900

    [도형] 도커 빌드용 코드 추가

--- one-click-deploy.sh
+++ one-click-deploy.sh
@@ -1,8 +1,11 @@
 # !/src/sh
 #!주의) 빌드 도커 배포용 코드 + mac 용이니 실행 하지 마세요.
 
+docker build --platform linux/amd64 -t hbgoguma-project .
+
+docker tag hbgoguma-project mrdos89/hbgoguma-project
+
 # docker에서 push 올림
-docker push mrdos89/hbgogumaserver
 docker push mrdos89/hbgoguma-project
 
 # compose 폴더에서 .env에 맞춰 docker compose 실행
