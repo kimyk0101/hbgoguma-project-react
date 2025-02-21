49f5444c985796385a626922641949cbaca954fb
jensen-kor <lukawa1@naver.com>
2025-02-21 15:53:22 +0900

    [준희] 헤더수정

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,10 +9,22 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-container">
-        {/* 중앙: 로고 */}
-        <h1 className="header-logo" onClick={() => handleNavigation("/")}>
-          호박고구마
-        </h1>
+        {/* 왼쪽: 로고 */}
+        <div className="header-logo" onClick={() => handleNavigation("/")}>
+          <img
+            src="src/resources/images/sweet-potato-Filled.png"
+            alt="호박고구마 로고"
+            className="logo-image"
+          />
+          <h1>
+            호박고구마{" "}
+            <img
+              src="src/resources/images/sweet-potato-Filled.png"
+              alt="호박고구마 로고"
+              className="Elogo-image"
+            />
+          </h1>
+        </div>
 
         {/* 오른쪽: 회원가입 & 로그인 버튼 */}
         <div className="header-buttons">
