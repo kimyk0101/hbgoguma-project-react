0e6161d4c3b1dcfcc56cafebf20da4135dfa2c82
kimyk0101 <kimyk7131@gmail.com>
2025-02-27 18:56:42 +0900

    [연경] 상세페이지 검색창 수정

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,12 +1,28 @@
 import React, { useState } from "react";
-import PopularKeywords from "./PopularKeywords";
+// import PopularKeywords from "./PopularKeywords";
 import "../css/searchBar.css";
 import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
-const SearchBar = ({ searchTerm, setSearchTerm, onSearch }) => {
-  const handleKeyDown = (e) => {
+const SearchBar = ({ searchTerm, setSearchTerm, onSearch, onKeyPress }) => {
+  // const handleKeyDown = (e) => {
+  //   if (e.key === "Enter") {
+  //     console.log("검색 실행: ", searchTerm);
+  //     onSearch();
+  //   }
+  // };
+  const navigate = useNavigate();
+
+  const handleSearch = () => {
+    console.log("내가 입력한 검색어 : " + searchTerm);
+
+    navigate(`/?query=${searchTerm}`);
+    onSearch();
+  };
+
+  const handleKeyPress = (e) => {
     if (e.key === "Enter") {
-      onSearch();
+      handleSearch();
     }
   };
 
@@ -17,37 +33,17 @@ const SearchBar = ({ searchTerm, setSearchTerm, onSearch }) => {
         placeholder="검색어를 입력하세요..."
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-        onKeyDown={handleKeyDown} // Enter 키 이벤트 추가
+        // onKeyDown={onKeyPress} // Enter 키 이벤트 추가
+        onKeyDown={handleKeyPress}
         className="search-input"
       />
       <button className="search-button" onClick={onSearch}>
         <FaSearch />
       </button>
+      {/* 인기 검색어 표시 */}
+      {/* <PopularKeywords searchTerm={searchTerm} /> */}
     </div>
   );
 };
-// const SearchBar = ({ onSearch }) => {
-//   const [inputValue, setInputValue] = useState("");
-
-//   const handleSearchClick = () => {
-//     onSearch(inputValue);
-//   };
-
-//   return (
-//     <div className="search-container">
-//       <input
-//         type="text"
-//         placeholder="검색어를 입력하세요..."
-//         value={inputValue}
-//         onChange={(e) => setInputValue(e.target.value)}
-//         className="search-input"
-//       />
-//       <button className="search-button" onClick={handleSearchClick}>
-//         검색
-//       </button>
-//       <PopularKeywords onSelectKeyword={setInputValue} onSearch={onSearch} />
-//     </div>
-//   );
-// };
 
 export default SearchBar;
